901de01b06e890d022509e73fce9091aa03472f8
chaharshivam <chahar.shivam@yahoo.com>
2019-06-29 15:31:52 +0530

    Update timer and add media

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,33 +1,51 @@
-// // JS
-const play = document.getElementById('btn-play');
-const stop = document.getElementById('btn-stop');
-const counter = document.getElementById('counter');
-// // Event handler -> on input keydown -> store global -> input.value
-// // display value in clock
-// // event handled: on click -> start button
+const PLAY      = document.getElementById('btn-play');
+const STOP      = document.getElementById('btn-stop');
+const COUNTER   = document.getElementById('counter');
+const AUDIO     = new Audio('assets/sounds/ding.wav');
 
-function startTimer(duration, counter) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
+let interval
+    , currentDuration = 1500;
+
+function startTimer(duration, COUNTER) {
+    let timer = duration
+        , minutes
+        , seconds;
+    
+    interval = setInterval(function () {
+        // Calculate minutes
         minutes = Math.floor(timer / 60);
-        seconds = Math.floor(timer % 60);
+        // Calculate seconds
+        seconds = (timer % 60);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        // Pad the minutes with zero, if required
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        // Pad the seconds with zero, if required
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-        counter.textContent = minutes + ":" + seconds;
+        // Set inner HTML of the COUNTER to current duration
+        COUNTER.textContent = minutes + ":" + seconds;
+        // Calculate current duration to keep record of its value when timer is paused
+        currentDuration = minutes * 60 + seconds;
+        
+        if(currentDuration == 0) {
+            AUDIO.play();
+            clearInterval(interval);
+            currentDuration = 10;
+            return;
+        }
 
         if (--timer < 0) {
+            // make the timer equal to current duration
             timer = duration;
         }
     }, 1000);
 }
 
-play.addEventListener('click', function() {
-    var fiveMinutes = 60 * 5;
-    startTimer(1500, counter);
+PLAY.addEventListener('click', function() {
+    startTimer(currentDuration, COUNTER);
 });
 
-stop.addEventListener('click', function() {
+STOP.addEventListener('click', function() {
+    clearInterval(interval); 
     
 });
